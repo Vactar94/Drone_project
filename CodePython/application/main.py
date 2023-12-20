977f6ac1838593211524dd67f0959a8869f1c6fa
Ogama <113932665+Ogama-0@users.noreply.github.com>
2023-12-20 08:36:51 +0100

    maj du streem pour opti les perfs

--- CodePython/application/main.py
+++ CodePython/application/main.py
@@ -17,11 +17,8 @@ from code_python.telo import DRONE
 from code_python.notification import NOTIF_MANAGER
 
 
-
 Window.size = [360, 620]
 Window.OS = SYSTEM
-
-
 
 
 class The_app(App):
@@ -38,7 +35,6 @@ class The_app(App):
 
 
     def build(self):
-        
         #création des screens  
         notifs = crea_notif()
         ui_screen = UiScreen(name='ui', notifications=notifs.copy())
@@ -57,7 +53,8 @@ class The_app(App):
         self.sm.add_widget(parametre_screen)
         self.sm.add_widget(automatique_screen)
 
-        Clock.schedule_interval(self.update, 1.0 / 60.0)
+        Clock.schedule_interval(self.update_60, 1.0 / 60.0)
+        Clock.schedule_interval(self.update_30, 1.0 / 30.0)
         Clock.schedule_interval(self.check_long, 1.0 / 1.0)
         
         return self.sm
@@ -67,16 +64,19 @@ class The_app(App):
         print(self.seconde)
         UPDATE_MANAGER.update_all_1()
     
-    def update(self,dt) :
+    def update_30(self,dt) :
+        self.update_streem()
+        
+
+    def update_60(self,dt) :
         """main_loop avec toutes les updates"""
         self.update_notif()
-        self.update_streem()
         UPDATE_MANAGER.update_all_60()
         #print(DRONE.is_connected)
         
-        
     
     def update_streem(self) :
+        """update les images 30 fois par seconde et l'affiche sur le self.curent_screen.background qui est une image stocké dans box_background"""
         if self.sm.current_screen.streem_background :
             if self.dm.is_connected:
                 self.sm.current_screen.background = self.dm.get_image(self.sm.current_screen.background)
@@ -84,6 +84,7 @@ class The_app(App):
 
 
     def update_notif(self) :
+        """prend tout NOTIF_MANAGER.Waiting_notifications et les affiches sur le sm.curent_screen"""
         W_N = NOTIF_MANAGER.Waiting_notifications 
         for k in W_N.keys() :
             for i in W_N[k].keys() : 
@@ -95,6 +96,7 @@ class The_app(App):
         return self.dm.connect()
 
 class UiScreen(Better_Screen):
+    """setup un peu cocase pour avoir un kv.uix.screen et kv.uix.pagelayout"""
     
     def __init__(self, **kwargs):
         
@@ -108,11 +110,9 @@ class UiScreen(Better_Screen):
             for notif in self.notifications[k] :
                 self.add_widget(notif)
     
-
     def go_to(self,value,name_of_the_target_screen:str):
         self.manager.current = name_of_the_target_screen
-    
-    
+
 # -- les mains pages de l'app -- #
 class UI(PageLayout):
     def __init__(self,ui_screen,**kwargs):
@@ -124,8 +124,6 @@ class UI(PageLayout):
         
         self.add_widget(self.page1)
         self.add_widget(self.page2)
-
-
 
 class Screen_proj(Better_Screen):
 
@@ -160,17 +158,13 @@ class Screen_Controles(Screen_sous_menu) :
         self.box_background.size = (Window.size[0]*0.7, Window.size[1]*0.7)
 
         self.add_widget(self.box_background)
-        
-        
-        
+
 
 class Screen_Parametre(Screen_sous_menu) :
     def __init__(self,**kwargs):
         icone = Image(source="image/icone_button_parametre_bg.png")
         super().__init__(**kwargs, text_titre="Parametres", icone=icone, streem_background=False)
         self.add_widget(self.redu_layout)
-
-
 
 
 class Screen_Classique(Screen_sous_menu) :
